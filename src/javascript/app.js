3446a0b0f977a8ed61c51f491647828c5852d21b
Sandrico Provo <Sprovo@outlook.com>
2020-05-04 11:01:46 -0300

    Change: added form and search btn for inputs

---
+++ src/javascript/app.js
@@ -0,0 +1,29 @@
+(function(){
+  let searchedPlayer = document.querySelector('[name="search-name"]');
+  let searchedSeason = document.querySelector('[name="search-season"]');
+  let playerName = document.querySelector('.stat__player-name');
+  let playerNumber = document.querySelector('.stat__player-number');
+  let playerPosition = document.querySelector('.stat__position');
+  let playerTeam = document.querySelector('.stat__team');
+  let playerHeight = document.querySelector('.stat__height');
+  let playerPoints = document.querySelector('.stat__points');
+  let playerRebounds = document.querySelector('.stat__rebounds');
+  let playerAssists = document.querySelector('.stat__assists');
+  let playerSteal = document.querySelector('.stat__steals');
+  let playerBlocks = document.querySelector('.stat__blocks');
+  let playerMinsPG = document.querySelector('.stat__mpg');
+  let playerFgPct = document.querySelector('.stat__fgpct');
+  let player3ptPct = document.querySelector('.stat__3ptpct');
+  let playerFTPct = document.querySelector('.stat__ftpct');
+
+
+
+
+  // Event Listeners =====================================
+
+
+
+
+
+
+})();
