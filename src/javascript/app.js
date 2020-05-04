51c6dab068c48e35f525cbcdc00acbca66f56949
Sandrico Provo <Sprovo@outlook.com>
2020-05-04 11:48:35 -0300

    Update: fetchs data with user input

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,6 +1,8 @@
 (function(){
+  let inputForm = document.querySelector('.form__search')
   let searchedPlayer = document.querySelector('[name="search-name"]');
   let searchedSeason = document.querySelector('[name="search-season"]');
+  let searchSubmitBtn = document.querySelector('.form__submitbtn');
   let playerName = document.querySelector('.stat__player-name');
   let playerNumber = document.querySelector('.stat__player-number');
   let playerPosition = document.querySelector('.stat__position');
@@ -16,14 +18,35 @@
   let player3ptPct = document.querySelector('.stat__3ptpct');
   let playerFTPct = document.querySelector('.stat__ftpct');
 
+  let fetchPlayerData = async (firstName, lastName, season = 2019) => {
+    // fetch player details
+    let detailsReponse = await fetch(`https://www.balldontlie.io/api/v1/players?search=${firstName}_${lastName}`);
+    let { data: [ detailsJson ] } = await detailsReponse.json();
+    let playerID = detailsJson.id;
+
+    // fetch player season stats
+    let seasonStatsResponse = await fetch(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${playerID}`);
+    let { data: [ seasonStatsJson ] } = await seasonStatsResponse.json();
+  };
+
+  
+
+
+
+
 
 
 
   // Event Listeners =====================================
-
-
-
-
+  searchSubmitBtn.addEventListener('click', (event) => {
+    // prevents the form from submitting and refreshing the page
+    event.preventDefault();
+    // search values from user input
+    let [playerFirstName, playerLastName] = searchedPlayer.value.split(" ");
+    let searchSeason = searchedSeason.value;
+    // calling the fetch data function
+    fetchPlayerData(playerFirstName, playerLastName, searchSeason);
+  })
 
 
 })();
